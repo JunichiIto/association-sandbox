f26354b68b72cabd098313cb5eb7b6b7ff743628
Junichi Ito <jit@sonicgarden.jp>
2014-03-19 22:22:46 +0900

    Add comment spec.

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,4 +1,25 @@
 require 'spec_helper'
 
 describe Comment do
+  describe 'association' do
+    before :each do
+      @post = Post.create(text: 'こんにちは、伊藤です。')
+    end
+    context 'not associated' do
+      it 'does not have post' do
+        expect(Comment.new.post).to be_nil
+      end
+      it 'does not have post_id' do
+        expect(Comment.new.post_id).to be_nil
+      end
+    end
+    context 'associated' do
+      it 'has post' do
+        expect(@post.comments.build.post).to eq @post
+      end
+      it 'has post_id' do
+        expect(@post.comments.build.post_id).to eq @post.id
+      end
+    end
+  end
 end
