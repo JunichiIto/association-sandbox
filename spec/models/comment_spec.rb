159974170bc3e3f2834b27ece16131dad95145e1
Junichi Ito <jit@sonicgarden.jp>
2014-03-19 22:25:32 +0900

    Refactor spec.

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -6,19 +6,31 @@ describe Comment do
       @post = Post.create(text: 'こんにちは、伊藤です。')
     end
     context 'not associated' do
+      before :each do
+        @comment = Comment.new(text: '山田です。伊藤さん、こんにちは！')
+      end
       it 'does not have post' do
-        expect(Comment.new.post).to be_nil
+        expect(@comment.post).to be_nil
       end
       it 'does not have post_id' do
-        expect(Comment.new.post_id).to be_nil
+        expect(@comment.post_id).to be_nil
+      end
+      specify 'post does not have comments' do
+        expect(@post.comments.size).to eq 0
       end
     end
     context 'associated' do
+      before :each do
+        @comment = @post.comments.build(text: '山田です。伊藤さん、こんにちは！')
+      end
       it 'has post' do
-        expect(@post.comments.build.post).to eq @post
+        expect(@comment.post).to eq @post
       end
       it 'has post_id' do
-        expect(@post.comments.build.post_id).to eq @post.id
+        expect(@comment.post_id).to eq @post.id
+      end
+      specify 'post has a comment' do
+        expect(@post.comments.size).to eq 1
       end
     end
   end
