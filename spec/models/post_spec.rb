b8978c7965310aa8020fa8c2b21a1f5188a268d2
Junichi Ito <jit@sonicgarden.jp>
2014-03-20 05:47:41 +0900

    Add depends option

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,8 +1,19 @@
 require 'spec_helper'
 
 describe Post do
-  it "has comments" do
-    expect(Post.new.comments.count).to eq 0
+  describe 'association' do
+    it "has comments" do
+      expect(Post.new.comments.count).to eq 0
+    end
+    describe 'destroy comments' do
+      before :each do
+        @post = Post.create(text: 'こんにちは、伊藤です。')
+        @post.comments.create(text: '山田です。伊藤さん、こんにちは！')
+      end
+      it 'destroys comments too' do
+        expect{@post.destroy}.to change(Comment, :count).from(1).to(0)
+      end
+    end
   end
   describe 'validation' do
     context 'with text' do
