3f84c960018e75702bf7bc6fc599d5f73d4e9ac9
Junichi Ito <jit@sonicgarden.jp>
2014-03-19 22:00:52 +0900

    Remove unused parameter.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -68,6 +68,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:text, :post_id)
+      params.require(:comment).permit(:text)
     end
 end
