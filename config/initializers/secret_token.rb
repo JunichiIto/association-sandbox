ba2b7b692e645f44dd212ba38b1de7df23b03b6d
Junichi Ito <jit@sonicgarden.jp>
2014-03-19 21:03:24 +0900

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AssociationSandbox::Application.config.secret_key_base = 'e5b705dbd1f8e8c5d6860355d2ef441af7a5c50733c4614eadaf32d7aa897bbed017dad6d85ad1e22df084ab4fcccd146b9e1f925000f1a8ffb5a4595e256920'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AssociationSandbox::Application.config.session_store :cookie_store, key: '_association-sandbox_session'
