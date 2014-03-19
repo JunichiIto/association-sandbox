d8b715b64f757ea7c7e33e0f905be5eb0395b938
Junichi Ito <jit@sonicgarden.jp>
2014-03-20 05:27:50 +0900

    Add examples for validation of Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
   has_many :comments
+
+  validates :text, presence: true
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,4 +4,16 @@ describe Post do
   it "has comments" do
     expect(Post.new.comments.count).to eq 0
   end
+  describe 'validation' do
+    context 'with text' do
+      it 'is valid' do
+        expect(Post.new(text: 'Hello')).to be_valid
+      end
+    end
+    context 'without text' do
+      it 'is invalid' do
+        expect(Post.new(text: nil)).to have(1).error_on(:text)
+      end
+    end
+  end
 end
